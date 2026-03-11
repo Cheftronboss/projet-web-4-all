3b5d33215a97af33d0f1177d11cd23b994b442b3
Romain <romainbeche@icloud.com>
2026-03-11 23:02:12 +0100

    Add Twig setup, front controller and templates
    Scaffold project for Twig-based rendering: add composer.json with Twig dependency, create public/index.php front controller that initializes Twig and simple page routing, add base template and component/page templates (moved/renamed into templates/), move stylesheet to public/css, and add .gitignore to ignore vendor. Prepares app structure for rendering pages with Twig.

---
+++ public/index.php
@@ -0,0 +1,36 @@
+<?php
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$loader = new \Twig\Loader\FilesystemLoader(__DIR__ . '/../templates');
+$twig = new \Twig\Environment($loader, [
+    'cache' => __DIR__ . '/../cache',  // false en dev
+]);
+
+$page = $_GET['page'] ?? 'accueil';
+$allowedPages = [
+    'accueil', 
+    'compte-entreprise', 
+    'compte-etudiant', 
+    'compte-pilote', 
+    'creation-compte-etudiant', 
+    'creation-compte-pilote',
+    'creation-entreprise',
+    'creer-offre',
+    'modification-compte-entreprise',
+    'modification-compte-etudiant',
+    'modification-compte-pilote',
+    'modification-offre-terminee',
+    'modification-offre',
+    'suppression-compte-1',
+    'suppression-compte-2',
+    'suppression-offre'
+    ];
+
+if (!in_array($page, $allowedPages)) {
+    http_response_code(404);
+    $page = '404';
+}
+
+echo $twig->render("pages/{$page}.twig.html", [
+    'currentPage' => $page,
+]);
